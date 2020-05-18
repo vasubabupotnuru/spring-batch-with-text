2a538c3e9812a5aeab5aa2d8dfe2279f9d59d766
vasu <vasubabupotnuru@gmail.com>
2020-05-18 20:08:09 +0530

    Modified to txt from csv

--- src/main/java/com/techshard/batch/Application.java
+++ src/main/java/com/techshard/batch/Application.java
@@ -1,14 +1,36 @@
 package com.techshard.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {BatchAutoConfiguration.class})
+@EnableScheduling
 public class Application extends SpringBootServletInitializer {
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job importVoltageJob;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void perform() throws Exception {
+        JobParameters params = new JobParametersBuilder()
+                .addString("JobID", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+        jobLauncher.run(importVoltageJob, params);
     }
 
 }
